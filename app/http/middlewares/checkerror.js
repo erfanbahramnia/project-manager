32279559e957e253e926f50d692a2f277b967a71
erfan bahramnia <erfanbahramnia@gmail.com>
2023-04-23 20:53:30 +0430

    fix bug of the error message

--- app/http/middlewares/checkerror.js
+++ app/http/middlewares/checkerror.js
@@ -12,7 +12,7 @@ const expressValidator = (req, res, next) => {
             // save errors in a object
             result.errors.forEach(error => {
                 console.log(error)
-                errors[error.param] = error.msg || error.msg.message;
+                errors[error.param] = error.msg.message || error.msg ;
             });
             // sending erros
             res.json({
